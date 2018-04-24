4c59bbe59d6046d93e2739d2e5c245056da40235
James Digrazia <jtd2401@rit.edu>
2018-04-23 22:41:00 -0400

    Setup proxy for API calls, remove node modules folder

--- landing.js
+++ landing.js
@@ -4,8 +4,8 @@
 'use strict';
 
 //IIFE
-(function () { 
-    
+(function () {
+
     var particleOverlayColor = "#80D0E5";
     var previousUpdateTime = undefined;
     var animationID = 0;
@@ -14,11 +14,14 @@
     var canvasContainer = undefined;
     var searchButton = undefined;
     var queryBox = undefined;
-    
+    var resultsBox = undefined;
+
+    var cData = undefined;
     var previousSearchTerm;
+    var numResults = 5;
     //Charity Search API Key
     var apiKey = "cc68fc7689c5d78a918241ff8c0eb905";
-    
+
     //Particle Variables
     var maxParticles = 150;
     var preWarmCount = 100;
@@ -32,23 +35,24 @@
     var particleSpeed = 10;
     var particleTimer = 0;
     var particles = []; //Array of particle objects
-    
-    
+
+
     function init() {
         canvas = document.querySelector('canvas');
         ctx = canvas.getContext('2d');
         canvasContainer = document.querySelector('header');
         searchButton = document.querySelector('#searchbutton');
         queryBox = document.querySelector('input');
-        
+        resultsBox = document.querySelector('#resultsbox');
+
         searchButton.onclick = performSearch;
-        
-        
-        
+
+
+
         //Set canvas to fit container size
         canvas.width = canvasContainer.offsetWidth;
         canvas.height = canvasContainer.offsetHeight;
-        
+
         if(window.innerWidth >= 550) {
             particleLineRange = 25;
             maxParticles = 250;
@@ -59,35 +63,35 @@
             maxParticles = 150;
             preWarmCount = 100;
         }
-        
+
         //Initialize update timer
         previousUpdateTime = performance.now();
-        
+
         //PreWarm Particles
         preWarmParticles();
-        
+
         update();
     }
-    
+
     function update() {
         //Check Time Passed
         var dt = calculateDeltaTime();
-        
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        
+
         //Draw overlay
         ctx.fillStyle = particleOverlayColor;
         ctx.fillRect( 0, 0, canvas.width, canvas.height);
-        
+
         //Draw Particles and Lines
         ctx.globalAlpha = 1;
         drawParticles(ctx);
         drawParticleLines(ctx);
         createParticles(dt);
         moveParticles(dt, particleSpeed);
-        
+
         ctx.save();
-        
+
         //Draw text
         ctx.globalAlpha = 1;
         ctx.textAlign = "center";
@@ -97,38 +101,38 @@
         ctx.shadowBlur = 10;
     	ctx.font = "3em 'Jua', 'Comfortaa'";
     	ctx.fillText("Bubble Pheonix", canvas.width / 2, 20);
-    	
+
     	//ctx.fillStyle = "cyan";
         //ctx.shadowColor = "pink";
         ctx.shadowBlur = 10;
     	ctx.font = "2em 'Yellowtail'";
     	ctx.fillText("You can do some good", canvas.width / 2, 90);
         ctx.restore();
-        
+
         //Schedule call to update
         requestAnimationFrame(update);
     }
-    
-    
-    
+
+
+
     //Helper Functions
-    
+
     //Returns the time passed since the last time this function has been called
     function calculateDeltaTime() {
-		var now,fps;
-		now = performance.now(); 
-		fps = 1000 / (now - previousUpdateTime);
-		fps = clamp(fps, 12, 60);
-		previousUpdateTime = now; 
-		return 1/fps;
-	}
-	
+  		var now,fps;
+  		now = performance.now();
+  		fps = 1000 / (now - previousUpdateTime);
+  		fps = clamp(fps, 12, 60);
+  		previousUpdateTime = now;
+  		return 1/fps;
+  	}
+
 	/*
     Function Name: clamp(val, min, max)
     Author: Web - various sources
     Return Value: the constrained value
     Description: returns a value that is
-    constrained between min and max (inclusive) 
+    constrained between min and max (inclusive)
     */
     function clamp(val, min, max){
     	return Math.max(min, Math.min(max, val));
@@ -138,40 +142,40 @@
 
 
     //Particle Functions
-	
+
 	//Iterates through the array of particles and draws them at their current location
 	function drawParticles(ctx) {
 	    ctx.save();
-	    
+
 	    ctx.fillStyle = particleColor;
 	    ctx.shadowBlur = 10;
         ctx.shadowColor= particleShadowColor;
-	    
+
 	    //Draw particles
 	    for(var i = 0; i < particles.length; i++) {
 	        ctx.beginPath();
 	        ctx.arc(particles[i].x, particles[i].y, particleSize, 0, 2 * Math.PI);
 	        ctx.fill();
 	    }
-	    
+
 	    ctx.restore();
 	}
-	
+
 	function drawParticleLines() {
 	    ctx.save();
-	    
+
 	    ctx.strokeStyle = particleLineColor;
 	    ctx.shadowBlur = 5;
 	    ctx.shadowColor = particleLineShadowColor;
-	    
+
 	    for(var i = 0; i < particles.length; i++) {
 	        for(var j = 0; j < particles.length; j++) {
 	            if(i == j)
 	                continue;
-	       
+
 	            var xDist = Math.abs(particles[i].x - particles[j].x);
 	            var yDist = Math.abs(particles[i].y - particles[j].y);
-	            
+
 	            if(xDist < particleLineRange && yDist < particleLineRange) {
 	                ctx.beginPath();
 	                ctx.moveTo(particles[i].x, particles[i].y);
@@ -180,18 +184,18 @@
 	            }
 	        }
 	    }
-	    
+
 	    ctx.restore();
 	}
-	
+
 	//Create new particles
 	function createParticles(dt) {
 	    var timePerParticle = 1 / particleRate;
 	    //Check timer and max particles before creating a new particle
 	    if(particleTimer >= timePerParticle && particles.length < maxParticles) {
-	        
+
 	        //Generate particle variables
-	        //Generate on-canvas location variable 
+	        //Generate on-canvas location variable
             var x = Math.random() * canvas.width;
             var y = Math.random() * canvas.height;
             //Generate random particle speeds
@@ -199,28 +203,28 @@
             xSp -= particleSpeed;
             var ySp = Math.random() * particleSpeed * 2;
             ySp -= particleSpeed;
-            
+
             //Create new particle
             var p = new Particle(x, y, xSp, ySp);
-            
+
             //Add particle to particle list
             particles.push(p);
-	        
+
 	        //Reset particle timer
 	        particleTimer = 0;
 	    }
-	    else //Increment timer 
+	    else //Increment timer
 	        particleTimer += dt;
 	}
-	
+
 	//Move particles and delete them when they leave the screen
 	function moveParticles(dt, particleSpeed) {
 	    for(var i = 0; i < particles.length; i++) {
-	        
+
 	        //Move particle
 	        particles[i].x += particles[i].xSpeed * dt;
 	        particles[i].y += particles[i].ySpeed * dt;
-	        
+
 	        //Remove particles when they move offscreen
 	        if(particles[i].x < 0 || particles[i] > canvasContainer.offsetWidth
 	        || particles[i].y < 0 || particles[i].y > canvasContainer.offsetHeight)
@@ -230,7 +234,7 @@
 	        }
 	    }
 	}
-    
+
     //Particle object constructor
     function Particle( x, y, xSpd, ySpd) {
         this.x = x;
@@ -242,8 +246,8 @@
     //Create as many particles as our prewarm count
     function preWarmParticles() {
         while(preWarmCount > particles.length) {
-            
-            //Generate on-canvas location variable 
+
+            //Generate on-canvas location variable
             var x = Math.random() * canvas.width;
             var y = Math.random() * canvas.height;
             //Generate random particle speeds
@@ -251,56 +255,123 @@
             xSp -= particleSpeed;
             var ySp = Math.random() * particleSpeed * 2;
             ySp -= particleSpeed;
-            
+
             //Create new particle
             var p = new Particle(x, y, xSp, ySp);
-            
+
             //Add particle to particle list
             particles.push(p);
             //console.dir(particles);
         }
     }
 
-    
-    
+
+
     //Search Functions
-    
+
     function performSearch() {
         //Grab search term
         var searchTerm = queryBox.value;
-        
+
         //Prevent searching for a query that is currently displayed
-        if(searchTerm == previousSearchTerm) 
+        if(!searchTerm || searchTerm.length === 0 || searchTerm == previousSearchTerm)
             return;
-        
+
         //Parse string and split it into our search terms
         var queries = searchTerm.split(", ");
         var city = queries[0];
         var state = queries[1];
-        
+
         //Make API request
-        var apiCallLink = "https://data.orghunter.com/v1/charitysearch?user_key=" + apiKey + "&city=" + city + "&state=" + state;
-        
-        $.ajax({
-            crossDomain: true,
-            dataType: 'json',
-			url: apiCallLink,
-			headers: {
-			    'Access-Control-Allow-Origin': '*',
-			},
-			success: appendCharities,      
-        });
-        
+        var apiCallLink = "https://data.orghunter.com/v1/charitysearch?user_key=";
+        apiCallLink += apiKey + "&city=" + city + "&state=" + state +
+        "&eligible=1" + "&rows=" + numResults;
+
+        apiCallLink = encodeURIComponent(apiCallLink);
+
+        var url = "http://127.0.0.1:3000?url=" + apiCallLink;
+
+        console.dir(url);
+
+        var xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+          //parseCharities(xhr.responseText);
+
+          var oldResults = resultsbox.children;
+          var numOldResults = oldResults.length;
+
+          //Fade out results and call the charity parsing function when the
+          //last element fades
+          for(var result of oldResults) {
+            $(result).fadeOut(400);
+          }
+
+          parseCharities(xhr.responseText);
+
+        };
+
+        xhr.open('GET', url);
+        xhr.send();
     }
 
-    var appendCharities = function(charities) {
-        console.dir(charities.data);
+    function parseCharities(charities) {
+      charities = JSON.parse(charities);
+      cData = charities.data;
+
+      //console.dir(cData.length);
+      //console.dir(cData[0]);
+      for(var i = 0; i < cData.length; i++) {
+        var charityName = cData[i].charityName;
+        //Trim string down to 20 characters
+        if(charityName.length > 20)
+          charityName = charityName.substring(0, 20);
+
+        //Add up to three categories
+        var category = "";
+        for(var j = 0; i < j; j++) {
+          if(cData[i].category[j]) {
+            category += cData[i].category[j];
+            if(j < 3)
+            category += ", ";
+          }
+        }
+        //Trim string down to 20 characters
+        if(category.length > 20)
+          category = category.substring(0, 20);
+
+        var city = cData[i].city;
+        var state = cData[i].state;
+        var donationUrl = cData[i].donationUrl;
+
+        appendCharity( charityName, category, city, state, donationUrl);
+      }
+    }
+
+    function appendCharity( cName, cat, city, state, url) {
+      var searchItemElement = document.createElement('div');
+      var siName = document.createElement('span');
+      var siCategory = document.createElement('span');
+      var siCityState = document.createElement('span');
+      var siLink = document.createElement('a');
+
+      siName.innerHTML = cName;
+      siCategory.innerHTML = cat;
+      siCityState.innerHTML = city + ", " + state;
+      siLink.href = url;
+      siLink.innerHTML = "Donation Link";
+
+      searchItemElement.appendChild(siName);
+      searchItemElement.appendChild(siCategory);
+      searchItemElement.appendChild(siCityState);
+      searchItemElement.appendChild(siLink);
+
+      resultsBox.appendChild(searchItemElement);
     }
 
     function resize() {
         canvas.width = canvasContainer.offsetWidth;
         canvas.height = canvasContainer.offsetHeight;
-        
+
         if(window.innerWidth >= 550) {
             particleLineRange = 25;
             maxParticles = 250;
@@ -311,10 +382,10 @@
             maxParticles = 150;
             preWarmCount = 100;
         }
-        
+
         preWarmParticles();
     }
-    
+
     //When the orientation changes, call the resize function
     // Then make a callback to the resize function to use updated element values
     function resizeOC() {
@@ -326,4 +397,3 @@
     window.onresize = resize;
     window.addEventListener("orientationchange", resizeOC);
 })();
-
---
+++ server/proxy.js
@@ -0,0 +1,77 @@
+var http = require('http');
+var fs = require("fs");
+var queryString = require('querystring');
+var requestHandler = require('request');
+
+var ip = process.env.IP;
+var port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+var index = fs.readFileSync(__dirname + "/../landing.html");
+var styles = fs.readFileSync(__dirname + "/../landingstyles.css");
+var landingjs = fs.readFileSync(__dirname + "/../landing.js");
+
+var responseHeaders = {
+    "access-control-allow-origin": "*",
+    "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "access-control-allow-headers": "Content-Type, accept",
+    "access-control-max-age": 10,
+    "Content-Type": "application/json"
+};
+
+//Method to handle our page requests. The HTTP request from the browser will be
+//automatically passed in as request. The pre-formatted response object will be
+//automatically passed in as response so we can add to it and send it back.
+function onRequest(request, response) {
+
+    if(request.url === "/") {
+      response.writeHead(200, { "Content-Type" : "text/html"} );
+      response.write(index);
+      response.end();
+      return;
+    } else if (request.url === "/landingstyles.css") {
+      response.writeHead(200, { "Content-Type" : "text/css"} );
+      response.write(styles);
+      response.end();
+      return;
+    } else if (request.url === "/landing.js") {
+      response.writeHead(200, { "Content-Type" : "application/javascript"} );
+      response.write(landingjs);
+      response.end();
+      return;
+    }
+    //Split after the ? mark to get the query string (key=value pairs)
+    var query = request.url.split('?')[1];
+    var params = queryString.parse(query);
+
+    if(!params.url) {
+      response.writeHead(400, responseHeaders);
+
+      var responseMessage = {
+        message: "Missing url parameter in request"
+      };
+
+      response.write(JSON.stringify(responseMessage));
+      response.end();
+      return;
+    }
+
+    try{
+      response.writeHead(200, responseHeaders);
+      requestHandler(params.url).pipe(response);
+    }
+    catch(exception) {
+      console.dir(exception);
+      response.writeHead(500, responseHeaders);
+
+      var responseMessage = {
+        message: "Error connecting to server. Check url and arguments for proper formatting"
+      }
+
+      response.write(JSON.stringify(responseMessage));
+      response.end();
+    }
+
+}
+
+http.createServer(onRequest).listen(port);
+console.log("Listening on " + ip + ":" + port);
